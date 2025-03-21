ce5ce95b0e963b04b4ed86f381043a42b4dacfd7
Ben Lasher <113943616+BenLasher@users.noreply.github.com>
2025-03-20 20:36:24 -0400

    Create moc.py

---
+++ src/moc.py
@@ -0,0 +1,49 @@
+for i in():
+    #TODO 
+    print(i)
+    i+= 1
+
+
+
+"""
+i will be an array of values equaly spaced along the mach cone
+
+while TODO if the following x is past the end x chaordinate of the surface then terminitate the loop
+        TODO check to find the x chord asociated with the found points
+
+    for i
+        take each point (i,o) \\These points are on the mach cone
+
+        for j \\ At each point along the i axis continue down finding many point 3s untill i == j
+           
+            TODO solve for point 3 with points 1 and 2
+
+            \\ (i,j-1) is point 1
+            \\ (i-1,j) is point 2
+
+            TODO return point 3
+            TODO set point 3 into the mesh storer
+
+            if j == i
+                then the point is on the surface of the expansion hyperbola
+                TODO set bool to true
+                TODO return bool value
+
+        TODO Obtain the x value ascociated with the final (i,j) point
+    
+        i += 1
+        
+
+
+Line 17 for loop might be able to be replaced by a go untill i == j find each j point adding j
+
+
+for i untill i == j
+    #solve for point 3 with point 1 and 2
+
+    if i == j
+        #set bool to true
+        #tag and return
+
+    j ++
+"""
