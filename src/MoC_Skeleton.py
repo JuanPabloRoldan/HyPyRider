00542deb4c213476419f1d29568eefa6d3a32d70
Juan P. Roldan <juanproldan2003@gmail.com>
2025-03-25 13:58:15 -0400

    WIP: adding solver to the workflow

--- src/MoC_Skeleton.py
+++ src/MoC_Skeleton.py
@@ -1,4 +1,4 @@
-from method_of_characteristics import *
+from method_of_characteristics import FlowProperties, Point, AxisymmetricMOC
 import newton_raphson
 import NR_initial_guess
 
@@ -7,16 +7,18 @@ import numpy as np
 
 class MoC_Skeleton:
     
-    def __init__(self):
+    def __init__(self, Mach):
         """
         Initializes the MoC Skeleton class.
         """
-        
-        self.method_of_characteristics = method_of_characteristics()
+        self.flow_props = FlowProperties()
+        self.moc_solver = AxisymmetricMOC(self.flow_props)
         self.newton_raphson = newton_raphson()
-        self.NR_inital_guess = NR_initial_guess()
+        self.NR_initial_guess = NR_initial_guess()
 
-    def MoC_Skeleton():
+        self.Mach = Mach
+
+    def MoC_Mesher(self, leading_vertex):
         """
         Skeletal outline of the method of characteristics process. Insert functions and methods as needed 
         
@@ -28,25 +30,42 @@ class MoC_Skeleton:
         """
 
         i_max = 30
+        delta_s = 0.1
 
         # initialize square matrix to hold mesh
-        moc_mesh = np.zeroes(i_max, i_max)
+        moc_mesh = np.zeros(i_max, i_max)
         
-        # init_point = # grab known point on the leading vertex
-        # moc_mesh[0][0] = init_point
+        x0, r0 = leading_vertex
+        init_point = Point(x0, r0, 0, self.Mach, self.flow_props)
+        moc_mesh[0][0] = init_point
+
+        mu = init_point.mu
 
         for i in range(1, i_max):
+            # passing i as some i * delta | grab known point on the Mach cone
+            x_i = x0 + i * delta_s * np.cos(mu)
+            r_i = r0 + i * delta_s * np.sin(mu)
 
-            # cone_point = # passing i as some i * delta | grab known point on the Mach cone
-            # moc_mesh[i][0] = cone_point
+            moc_mesh[i][0] = cone_point = Point(x_i, r_i, 0.0, self.M, self.flow_props)
 
             for j in range(1, i): 
                 # for every internal point j along a line i
 
-                # moc_mesh[i][j] = solve_moc(moc_mesh[i][j-1].right, moc_mesh[i-1][j].left)
-    
+                J, F = moc_solver.compute_jacobian(moc_mesh[i][j-1], moc_mesh[i-1][j])
+                
             # once broken out of j loop, necessarily at a wall (ie j == 1)
-            # moc_mesh[i][i] = solve_moc(moc_mesh[i][i-1].right, is_wall=True)
+            # moc_mesh[i][i] = solve_moc(moc_mesh[i][i-1], is_wall=True)
 
             # if moc_mesh[i][i].x >= cone_length
             #     break
+
+if __name__ == "__main__":
+    Mach_number = 10.0  # example input
+
+    leading_vertex = [3.5010548, 3.5507]
+
+    moc_solver = MoC_Skeleton(Mach=Mach_number)
+    mesh = moc_solver.MoC_Mesher(leading_vertex)
+
+    # Basic print to confirm structure
+    print("MoC mesh initialized. Example corner entry:", mesh[0][0])
