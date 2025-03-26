56c90a0e26e85015b9cfd53e6367ccd953c36dd5
Juan P. Roldan <juanproldan2003@gmail.com>
2025-03-26 19:23:12 -0400

    modified NR init. guess to modify guesses at the surface

--- src/NR_initial_guess.py
+++ src/NR_initial_guess.py
@@ -2,15 +2,54 @@
 # theta_1, mu_1, M1, x1, y1, and nu1. We are also given theta_2, mu_2, M2, x2, y2, and nu2. 
 # We are importing the known conditions from another file to use in this file.
 import numpy as np
-def find_unknown_conditions_3(theta_1, mu_1, M1, x1, y1, nu1, theta_2, mu_2, M2, x2, y2, nu2):
-    # Defining the function to find the unknown conditions. All the unknown conditions are averages of the two initial conditions.
+from method_of_characteristics import Point
+from method_of_characteristics import FlowProperties
 
-    theta_3 = (theta_1 + theta_2) / 2
-    mu_3 = (mu_1 + mu_2) / 2
-    M3 = (M1 + M2) / 2
-    x3 = (x1 + x2) / 2
-    y3 = (y1 + y2) / 2
-    nu_3 = (nu1 + nu2) / 2
+def get_init_NR_guess(p1, p2, is_wall=False):
+    """
+    Computes an initial guess for NR based on averages of two known flow Points.
 
-    # Create xo vector for all the solved conditions.
-    xo = np.array([theta_3, mu_3, M3, x3, y3, nu_3])
+    Parameters:
+    p1, p2: Point
+        Known flow points along the C- and C+ characteristics.
+
+    Returns:
+    numpy.ndarray
+        Initial guess vector [theta3, nu3, mu3, M3, r3, x3]
+    """
+
+    theta2 = p2.theta
+    nu2 = p2.nu
+    mu2 = p2.mu
+    M2 = p2.M
+    r2 = p2.r
+    x2 = p2.x
+
+    if is_wall:
+
+        flow_solver = FlowProperties()
+
+        M3 = 1.05 * M2
+        mu3 = flow_solver.mach_angle(M3)
+        nu3 = flow_solver.prandtl_meyer(M3)
+        theta3 = nu3 + nu2
+        r3 = 0.98 * r2
+        x3 = 1.05 * x2
+
+        return np.array([theta3, nu3, mu3, M3, r3, x3])
+    
+    theta1 = p1.theta
+    nu1 = p1.nu
+    mu1 = p1.mu
+    M1 = p1.M
+    r1 = p1.r
+    x1 = p1.x
+
+    theta3 = 0.5 * (p1.theta + p2.theta)
+    nu3    = 0.5 * (p1.nu + p2.nu)
+    mu3    = 0.5 * (p1.mu + p2.mu)
+    M3     = 0.5 * (p1.M  + p2.M)
+    r3  = 0.5 * (p1.r  + p2.r)
+    x3  = 0.5 * (p1.x  + p2.x)
+
+    return np.array([theta3, nu3, mu3, M3, r3, x3])
