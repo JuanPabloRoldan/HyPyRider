76dd31cd64802a830af4154193bf781743c15cd1
Ben Lasher <113943616+BenLasher@users.noreply.github.com>
2025-04-01 10:32:16 -0400

    Added branch w/logic for using the curve instead of mach cone

--- src/MoC_Skeleton.py
+++ src/MoC_Skeleton.py
@@ -1,3 +1,42 @@
+'''
+Import All
+
+class MoC_Skeleton:
+    Initialize(Mach)
+        Create instance of FlowProperties
+        Create instance of AxissummetricMOC with FlowProperties
+        Set Mach number
+
+    Function MoC_Mesher(leading_vertex)
+        Set max iterations
+        Set shock angle delta_s
+        Initialize moc_mesh
+        Set first point [0][0] in moc_mesh with asociated values
+        Set inital mu value
+        i = formula of a known curve
+
+        For each value along function f(x) = (i,j):
+            Find following point off of previous point and mu
+            Assign point into moc_mesh[i][0]
+
+            At wall point i == j
+                Get points 1, 2 at wall boundry
+                Compute inital guess
+                Newton-Raphson solver (is_wall = true)
+                Store created point into moc_mesh[i][i]
+                
+
+            For j in range(1, i):
+                For every point j along a line i
+                Get previous points 1, 2
+                Compute inital guess
+                Newton-Raphson solver
+                Store created point into moc_mesh[i][i]
+
+            If x of wall point >= 9, break the loop (exit condition)
+
+        Return generated moc_mesh
+'''
 from method_of_characteristics import FlowProperties, Point, AxisymmetricMOC
 from newton_raphson import newton_raphson_system
 from NR_initial_guess import get_init_NR_guess
@@ -17,7 +56,18 @@ class MoC_Skeleton:
         # self.NR_initial_guess = NR_initial_guess()
 
         self.Mach = Mach
+        
+    def curve(x):
+        curve = 0.0302703*x^2 - 0.211956*x - 3.17966
 
+    def curve_xypoint_fori(i):
+        curve_xypoints = np.array([
+            [-3.55, -3.53, -3.49, -3.43, -3.34, -3.22, -3.08, -2.91, -2.72, -2.5],
+            [-1, -1.01, -1.05, -1.11, -1.2, -1.32, -1.46, -1.63, -1.83, -2.05]
+        ])
+
+        return curve_xypoints[:,i+1]
+        
     def MoC_Mesher(self, leading_vertex):
         i_max = 30
         delta_s = 0.1
@@ -27,14 +77,32 @@ class MoC_Skeleton:
         x0, r0 = leading_vertex
         init_point = Point(x0, r0, 0, self.Mach, self.flow_props)
         moc_mesh[0][0] = init_point
-
         mu = init_point.mu
 
-        for i in range(1, i_max):
+        for i in range(1,10):
             x_i = x0 + i * delta_s * np.cos(mu)
             r_i = r0 + i * delta_s * np.sin(mu)
 
             moc_mesh[i][0] = Point(x_i, r_i, 0.0, self.Mach, self.flow_props)
+
+            # Wall point
+            P1 = moc_mesh[i][i - 1]
+            P2 = moc_mesh[i - 1][i - 1]
+            guess = get_init_NR_guess(P1, P2, is_wall=True)
+
+            solution = newton_raphson_system(
+                lambda v: self.moc_solver.evaluate(v, P1, P2, is_wall=True),
+                lambda v: self.moc_solver.jacobian(v, P1, P2, is_wall=True),
+                guess
+                )
+
+            new_point = Point(
+                x=solution[5], r=solution[4],
+                theta=solution[0], M=solution[3],
+                flow_props=self.flow_props
+                )
+
+            moc_mesh[i][i] = new_point
 
             for j in range(1, i):
                 P1 = moc_mesh[i][j - 1]
@@ -55,25 +123,6 @@ class MoC_Skeleton:
                 )
 
                 moc_mesh[i][j] = new_point
-
-            # Wall point
-            P1 = moc_mesh[i][i - 1]
-            P2 = moc_mesh[i - 1][i - 1]
-            guess = get_init_NR_guess(P1, P2, is_wall=True)
-
-            solution = newton_raphson_system(
-                lambda v: self.moc_solver.evaluate(v, P1, P2, is_wall=True),
-                lambda v: self.moc_solver.jacobian(v, P1, P2, is_wall=True),
-                guess
-                )
-
-            new_point = Point(
-                x=solution[5], r=solution[4],
-                theta=solution[0], M=solution[3],
-                flow_props=self.flow_props
-                )
-
-            moc_mesh[i][i] = new_point
 
             if moc_mesh[i][i].x >= 9:
                 break
