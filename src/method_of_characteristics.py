c5255b5c4ec12f3f4303e1782d1691e84264d3d1
courtneypast <courtney.jean5313@gmail.com>
2025-04-01 14:35:51 -0400

    Update method_of_characteristics.py
    Changed F to be + instead of - on the left running C+ Compat char.

--- src/method_of_characteristics.py
+++ src/method_of_characteristics.py
@@ -316,7 +316,7 @@ class AxisymmetricMOC:
             J[4] = -(4 * point.r)/(((r3 + point.r)**2) * (C2 + np.cot(C4)))
             # J[5] = 0
 
-            F = C1 - ((2 * C3)/(C2 + np.cot(C4)))
+            F = C1 + ((2 * C3)/(C2 + np.cot(C4)))
         return J, F
 
     def _jacobian_mach_angle(self, vars):
